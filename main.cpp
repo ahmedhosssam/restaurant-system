4d2b034403974a6c7cf69e1f09b4f06a9eaa2068
Ahmed Hossam <hoss4614@gmail.com>
2023-08-10 10:08:04 +0300

    ignore a.out

--- main.cpp
+++ main.cpp
@@ -6,24 +6,21 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
     int c = -1;
     Manager a;
-    while(c != 0)
-    {
-            cout << "Main menu: \n\n"
-            << "  1. Kitchen\n"
-            << "  2. Make Order\n"
-            << "  3. Display Menu\n"
-            << "  4. Report\n"
-            << "  0. Exit\n"
-            << "\nEnter your choice: ";
+    while (c != 0) {
+        cout << "Main menu: " << endl << endl
+             << "  1. Kitchen" << endl
+             << "  2. Make Order" << endl
+             << "  3. Display Menu" << endl
+             << "  4. Report" << endl
+             << "  0. Exit" << endl
+             << "Enter your choice: ";
 
         cin >> c;
 
-        switch(c)
-        {
+        switch (c) {
         case 1:
             system("clear");
             a.kitchen();
@@ -44,9 +41,8 @@ int main()
             system("clear");
             return 0;
         default:
-            cout << "\nInvalid input! please try again...";
+            cout << "Invalid input! please try again...";
             sleep(3);
-
         }
     }
 
