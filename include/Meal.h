27a2157d8f637c0cb592cf25bff07b4b9156dc63
Ahmed Hossam <hoss4614@gmail.com>
2023-08-12 13:09:40 +0300

    refactor Meal class

--- include/Meal.h
+++ include/Meal.h
@@ -14,14 +14,10 @@ class Meal {
         Meal(string name, int quantity, Stock* s); // to choose ingredients from Stock* s
         Meal(string name, int quantity, vector<Ingredient*> inglist, Stock* s);
         Meal(Stock* s);
-        void updateMealIng(); // done
-        void setData(); // done
-        void calcPrice(); // done
-        void getData(); // done
-        void setOrderQuantity(int q);
-        int getOrderQuantity();
-        int getID();
-        int getMealIng();
+        void updateMealIng();
+        void setData();
+        void calcPrice();
+        void getData();
         int getQuantity();
         double getPrice();
         string getName();
@@ -35,7 +31,6 @@ class Meal {
 
         string name; // name of meal
         int quantity; // quantity of meal
-        int orderQuantity;
         double total_price = 0.00; // total price of meal = total of ingPrice
         vector<Ingredient*> inglist;
         Stock* ptr_s;
